e32fe234097e2a9ab59f20f7d86f848d481fb441
iXpleew <xpleew16@gmail.com>
2026-07-24 17:44:10 +0000

    [test]: added method to print first rows

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+
+def print_sample_data():
+    data_set = pd.read_csv("data/raw/train_transaction.csv")
+    print(data_set.head(10))
+
+
+def main():
+    print_sample_data()
+
+
+if __name__ == "__main__":
+    main()
