17097c5be19372a9be75ab3a48ff989406cbd403
iXpleew <xpleew16@gmail.com>
2026-07-26 19:39:41 +0000

    [cleaning]: added check_all_values() method to print value counts of specific column

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+def check_all_values(dset: pd.Series):
+    print(dset.value_counts())
 
 def print_sample_data():
     data_set = pd.read_csv("data/train/train_transaction.csv")
@@ -8,7 +10,11 @@ def print_sample_data():
 
 
 def main():
-    print_sample_data()
+    data_set = pd.read_csv("data/train/train_transaction.csv")
+    pd.set_option("display.max_columns", None)
+    
+    print("Is fraud:")
+    check_all_values(data_set["isFraud"])
 
 
 if __name__ == "__main__":
