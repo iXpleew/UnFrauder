6af88c98b48111638897a03ad7db304f98959de5
iXpleew <xpleew16@gmail.com>
2026-08-02 17:36:38 +0000

    [data]: created vector that contains diffs of every column

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -5,7 +5,8 @@ def main():
     data_set = pd.read_csv("data/train/train_transaction.csv")
     pd.set_option("display.max_columns", None)
     missing_values = data_set.isna().groupby(data_set["isFraud"]).mean()
-    print(missing_values)
+    diff_vector = missing_values.loc[1] - missing_values.loc[0]
+    print(diff_vector)
 
 
 if __name__ == "__main__":
