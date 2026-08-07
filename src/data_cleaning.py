8920d98e75121166820309d4f0ec7f32e73b1f93
iXpleew <xpleew16@gmail.com>
2026-08-07 17:50:38 +0000

    [data]: showed how many records does the data have

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -5,10 +5,14 @@ import numpy as np
 def main():
     data_set = pd.read_csv("data/train/train_transaction.csv", chunksize=5_000)
     how_many_nans = 0
+    records_number = 0
+
     for chunk in data_set:
+        records_number += chunk.shape[0]
         how_many_nans += chunk.isna().groupby(chunk["isFraud"]).sum()
     
     print(how_many_nans)
+    print(records_number)
     # missing_values = data_set.isna().groupby(data_set["isFraud"]).mean()
     #diff_vector = missing_values.loc[1] - missing_values.loc[0]
     #print(diff_vector)
