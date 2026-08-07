ebb3cd8de38ea80f4372754191d61d558c5b8ddc
iXpleew <xpleew16@gmail.com>
2026-08-07 17:42:30 +0000

    [data]: implemented for loop for summing nans using chunksize

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import numpy as np
 
 
 def main():
-    data_set = pd.read_csv("data/train/train_transaction.csv", chunksize=5000)
+    data_set = pd.read_csv("data/train/train_transaction.csv", chunksize=5_000)
+    how_many_nans = 0
     for chunk in data_set:
-        pass    
-    print(data_set.min())
-    print(data_set.max())
-
+        how_many_nans += chunk.isna().groupby(chunk["isFraud"]).sum()
+    
+    print(how_many_nans)
     # missing_values = data_set.isna().groupby(data_set["isFraud"]).mean()
     #diff_vector = missing_values.loc[1] - missing_values.loc[0]
     #print(diff_vector)
