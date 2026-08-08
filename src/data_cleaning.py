a5b626ebb78cd03b13459e7f62ffa8b4aa0cd30d
iXpleew <xpleew16@gmail.com>
2026-08-08 12:12:42 +0000

    [structure]: added structure for code to make t less complex

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-def main():
-    data_set = pd.read_csv("data/train/train_transaction.csv", chunksize=5_000)
+# data set is sortd, now it's time for splitting data set
+
+def split_dataset(data_set: pd.DataFrame):
+    pass
+
+
+def data_analysis(data_set: pd.DataFrame):
     how_many_nans = 0
     records_number = 0
 
@@ -12,11 +17,17 @@ def main():
         new_set = chunk.sort_values(by=["TransactionDT"])
         print(new_set["TransactionDT"][100])
         break
+
         records_number += chunk.shape[0]
         how_many_nans += chunk.isna().groupby(chunk["isFraud"]).sum()
     
     print(how_many_nans)
     print(records_number)
+
+
+def main():
+    data_set = pd.read_csv("data/train/train_transaction.csv", chunksize=5_000)
+    data_analysis(data_set)
     # missing_values = data_set.isna().groupby(data_set["isFraud"]).mean()
     #diff_vector = missing_values.loc[1] - missing_values.loc[0]
     #print(diff_vector)
