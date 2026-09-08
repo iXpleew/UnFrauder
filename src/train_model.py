754606f84d1138fe50a6cbb76b9f3eecb08b7713
iXpleew <xpleew16@gmail.com>
2026-09-08 13:40:31 +0000

    [model]: started to work on a downcasting the dataset

--- src/train_model.py
+++ src/train_model.py
@@ -3,18 +3,30 @@ import pdcast as pdc
 import numpy as np
 import xgboost as xgb
 
+FILE_PATH = "data/traindataset/train1.csv"
+
+
 def set_model_option():
     pd.set_option("display.max_columns", None)
     param["eval_metric"] = "auc"
 
 
+def build_dtypes():
+    column_names = pd.read_csv(FILE_PATH, nrows=0).columns.to_list()
+    dtype_dict = {}
+    for column in column_names:
+        series = pd.read_csv(FILE_PATH, usecols=[column])[column]
+
+        if series.dtype == "object":
+            dtype_dict[column] = "category"
+
 def main():
-    col_set = pd.read_csv("data/traindataset/train1.csv",nrows=0).columns.to_list()
-
-    train_set = pd.read_csv("data/traindataset/train1.csv", dtype=dtypes_dict)
-    set_model_option()  
+    trainset = pd.read_csv(FILE_PATH, nrows=2)  
+    dtypes = trainset.dtypes.replace({"object": "category", "int64": "int16", "float64": "float32"}).to_dict()
 
 
+    trainset = pd.read_csv(FILE_PATH, dtype=dtypes)
+    print(trainset.memory_usage(deep=True).sum())
 
 if __name__ == "__main__":
     main()
